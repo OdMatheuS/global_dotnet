8bafa40751143e3be235c117a2ca68f033613834
barfoo <barfoo@email.com>
2023-11-16 16:15:24 -0300

    feat:formulario de cadastro de usuario(MVC) + Btntaghelper

---
+++ Global/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Global.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Global.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private static IList<Usuario> _listaProdutos = new List<Usuario>();
+        private static int _idProduto = 0;
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Global/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace Global.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+        public decimal TempoSono { get; set; }
+        public bool PraticaEsporte { get; set; }
+        public string? HabitosSaude { get; set; }
+
+
+
+    }
+}
