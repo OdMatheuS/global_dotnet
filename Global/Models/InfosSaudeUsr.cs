e0086235f27c202ce69a9974963cf6726e4a6571
barfoo <barfoo@email.com>
2023-11-19 19:58:42 -0300

    update

--- Global/Models/InfosSaudeUsr.cs
+++ Global/Models/InfosSaudeUsr.cs
@@ -3,7 +3,7 @@
     public class InfosSaudeUsr
     {
         public int Id { get; set; }
-        public string HabitosSaude { get; set; }
+        public string? HabitosSaude { get; set; }
         public string? Alimentacao { get; set; }
         public decimal HorasSono{ get; set; }
         
