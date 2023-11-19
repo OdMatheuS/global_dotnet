a3a2e0138e819f93cbe1a6ddd1382fb12dd05039
barfoo <barfoo@email.com>
2023-11-19 14:16:10 -0300

    ajustes taghelper + controller enpoints e texto tela inicial

--- Global/Controllers/UsuarioController.cs
+++ Global/Controllers/UsuarioController.cs
@@ -6,7 +6,7 @@ namespace Global.Controllers
 {
     public class UsuarioController : Controller
     {
-        private static IList<Usuario> _listaProdutos = new List<Usuario>();
+        private static IList<Usuario> _listaUsuarios = new List<Usuario>();
         private static int _idProduto = 0;
 
         [HttpGet]
@@ -15,7 +15,68 @@ namespace Global.Controllers
             return View();
         }
 
-      
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            usuario.Id = ++_idProduto;
+            _listaUsuarios.Add(usuario);
+            TempData["msg"] = "Usuário cadastrado com sucesso!";
+            return RedirectToAction("Listar");
+        }
+
+        public IActionResult Listar()
+        {
+            return View(_listaUsuarios);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(int id)
+        {
+            var usuario = _listaUsuarios.FirstOrDefault(p => p.Id == id);
+            if (usuario != null)
+            {
+                _listaUsuarios.Remove(usuario);
+                TempData["msg"] = "Usuário removido com sucesso!";
+            }
+            else
+            {
+                TempData["msg"] = "Usuário não encontrado.";
+            }
+
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var usuario = _listaUsuarios.FirstOrDefault(p => p.Id == id);
+            if (usuario == null)
+            {
+                TempData["msg"] = "Usuário não encontrado.";
+                return RedirectToAction("Listar");
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Usuario usuario)
+        {
+            var usuarioExistente = _listaUsuarios.FirstOrDefault(p => p.Id == usuario.Id);
+            if (usuarioExistente != null)
+            {
+                usuarioExistente.Nome = usuario.Nome;
+                usuarioExistente.Idade = usuario.Idade;
+                TempData["msg"] = "Usuário atualizado com sucesso!";
+            }
+            else
+            {
+                TempData["msg"] = "Usuário não encontrado.";
+            }
+
+            return RedirectToAction("Listar");
+        }
+
 
         public IActionResult Index()
         {
